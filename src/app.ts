4241776e0d85be6c4dd6cc2eb68fae1e9b7b1bea
Precious Ibeagi <preciousaang@gmail.com>
2024-05-04 22:54:35 +0100

    wip

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log("OK this worksssss")
+import Express from 'express'
+const PORT = process.env.PORT || 3000
+const app = Express()
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+})
