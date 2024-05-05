a6457cc66ffaf09a6c0fee8b3caa46ff9cb6ca28
Precious Ibeagi <preciousaang@gmail.com>
2024-05-05 03:43:15 +0100

    wip

---
+++ src/tests/integration/user.test.ts
@@ -0,0 +1,9 @@
+// import { assert } from 'chai'
+// import { describe, it } from '@jest/globals'
+
+
+describe("User tests", () => {
+    it("1 + 1 should be 2", () => {
+        // assert.equal(1 + 1, 2)
+    })
+})
