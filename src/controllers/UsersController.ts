943c146ed682152e61778e30bdb86413f30ae018
Precious Ibeagi <preciousaang@gmail.com>
2024-05-05 01:28:50 +0100

    create users controller

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express"
+
+export const create = (req: Request, res: Response) => { }
+export const update = (req: Request, res: Response) => { }
+export const block = (req: Request, res: Response) => { }
